190ba394c0fa1ab62bb0a03cb4773271382fbcf1
emanmacario <emacario1997@hotmail.com>
2020-07-19 20:38:47 +1000

    Creates and partitions response string for user query

--- bot.py
+++ bot.py
@@ -60,7 +60,9 @@ def check_mentions(api, keywords, since_id):
 
             response = get_movie(movie_id)
             # pprint(response)
-            extract_info(response)
+            info = extract_info(response)
+            # pprint(info)
+            reply_to_user(info)
 
 
         # if any(keyword in tweet.text.lower() for keyword in keywords):
@@ -79,89 +81,169 @@ def check_mentions(api, keywords, since_id):
 
 
 
-def extract_info(response):
+def extract_info(movie):
     """
     Extracts key information from primary information given in the JSON response object
-    :param response: response JSON object
+    :param movie: response JSON object
     :return: dictionary
     """
-    """
-    === TWEET #1 ===
-    1. Release Date
-    2. Time
-    3. Star rating
-    4. Budget and gross (with percentage)
-    5. Genres
-    6. Production countries
-    
-    === TWEET #2 ===
-    1. Director
-    2. Written by
-    3. Starring
-    
-    === Tweet #3 ===
-    1. Description
-    """
-    # NB: Could do something like { k: v for k, v in dict.items() if k in keys }
+    # Keys for tweets
+    keys = ('original_title',
+            'release_date',
+            'runtime',
+            'vote_average',
+            'budget',
+            'revenue',
+            'overview',
+            'poster_path',
+            'backdrop_path')
 
     # Tweet #1
-    original_title = response['original_title']
-    release_date = response['release_date']
-    runtime = response['runtime']
-    vote_average = response['vote_average']
-    budget = response['budget']
-    revenue = response['revenue']
-    genres = [genre['name'] for genre in response['genres']]
-    production_countries = [country['iso_3166_1'] for country in response['production_countries']]
-    print(original_title)
-    print(release_date)
-    print(runtime)
-    print(vote_average)
-    print(budget)
-    print(revenue)
-    print(genres)
-    print(production_countries)
+    info = {key: value for key, value in movie.items() if key in keys}
+    info['genres'] = [genre['name'] for genre in movie['genres']]
+    info['production_countries'] = [country['iso_3166_1'] for country in movie['production_countries']]
+    info.update(get_credits(movie))
 
-    # Tweet #2
-    get_credits(response)
+    # Debugging
+    print("----PRINTING MOVIE INFO---")
+    pprint(info)
+    print('--------------------------')
 
-    # Tweet #3
-    overview = response['overview']
-    print("OVERVIEW:")
-    print(overview, '\n')
-
-    # Extra
-    poster_path = response['poster_path']
-    backdrop_path = response['backdrop_path']
-    print(f"Poster path: {poster_path}")
-    print(f"Backdrop path: {backdrop_path}")
+    return info
 
 
-
-def get_credits(response):
+def get_credits(movie):
     """
     Extracts credit information (e.g. Directors, actors, writers)
-    :param response: JSON response object
-    :return: dictionary
+    :param movie: movie response JSON object from TMDb API v3
+    :return: credits dictionary
     """
-    credits = response['credits']
+    credits = movie['credits']
     cast = credits['cast']
     crew = credits['crew']
 
     # Get top five actors and/or actresses
-    actors = [person['name'] for person in cast[:5]]
-    print("STARRING:")
-    pprint(actors)
-
+    actors = [person['name'].rstrip() for person in cast[:5]]
     # Get director(s)
-    print("DIRECTED BY:")
-    directors = [person['name'] for person in crew if person['job'] == 'Director']
-    pprint(directors)
-
+    directors = [person['name'].rstrip() for person in crew if person['job'] == 'Director']
     # Get writer(s)
-    writers = [person['name'] for person in crew if person['job'] == 'Writer']
-    pprint("WRITTEN BY:")
-    pprint(writers)
+    writers = [person['name'].rstrip() for person in crew if person['job'] in ('Writer', 'Screenplay')]
+
+    return {'actors': actors, 'directors': directors, 'writers': writers}
+
+
+
+
+
+from textwrap import dedent
+from utils.string_utils import formatted_date_str, formatted_num_str
+
+def reply_to_user(info):
+    """
+    Replies to a user query on Twitter
+    :param screen_name: user Twitter handle
+    :param in_reply_to_status_id: query status ID
+    :param info: movie info dictionary
+    :return: None
+    """
+    screen_name = "mantis_chad69"
+    reply_status = f"""
+    @{screen_name}
+    🎥 {info['original_title']}
+    🗓️ Released {formatted_date_str(info['release_date'])}
+    🕐 {info['runtime']} min
+    ⭐ {info['vote_average']} rating
+    💰 ${formatted_num_str(info['budget'])} budget
+    💵 ${formatted_num_str(info['revenue'])} box office
+    🎦 {', '.join(info['genres'])}
+    🌐 {', '.join(info['production_countries'])}
+    🎬 Directed by {', '.join(info['directors'])}
+    👪 Starring {', '.join(info['actors'])}
+    ✍ Written by {', '.join(info['writers'])}
+    📑 Overview: {info['overview']}"""
+
+    reply_status = dedent(reply_status)
+    print(repr(reply_status))
+
+    # api = create_api()
+    # api.update_status(reply_status)
+
+    #print(repr(dedent(reply_status)))
+    #print(f"Length of reply: {len(reply_status)} chars")
+    #print(reply_status.split('\n'))
+
+    partition_status(reply_status)
+
+
+
+
+
+def partition_status(status):
+    """
+    Splits a status string into statuses of 280 characters or less
+    (i.e. the limit imposed on tweet lengths by Twitter)
+    :param status: string
+    :return: list of strings
+    """
+    print("--- PARTITIONING STATUS ---")
+    # status = dedent(status)
+    lines = status.split('\n')
+
+    print("**SPLIT**")
+    for line in lines:
+        print(repr(line))
+
+    # List of status strings to be tweeted
+    statuses = []
+
+    # Divide the original string into sub-tweets
+    status = ''
+    for index, line in enumerate(lines):
+        # print(f"Line: {repr(line)}")
+        # print(f"Status: {repr(status)}")
+        if len(status + line) > 280:
+            statuses.append(status)
+            status = line
+        else:
+            status += line
+            if index == len(lines) - 1:
+                statuses.append(status)
+
+    pprint("**STATUSES**")
+    for status in statuses:
+        print(f"Length: {len(status)}")
+        print(repr(status))
+    return statuses
+
+
+
+
+
+def reply_to_self():
+    api = create_api()
+    status = api.update_status('Hello joe')
+
+    reply_id = status.id
+    screen_name = status.user.screen_name
+    api.update_status(f'@{screen_name} Hello, just replying to myself!', in_reply_to_status_id=reply_id)
+
+
+def upload_photo():
+    api = create_api()
+    try:
+        filename = 'images/vScen3pRHnbtlfNxErROpiM8ABm.jpg'
+        # filename = 'images/neds_dad.jpg'
+        with open(filename) as image:
+            media = api.update_with_media(filename, status="shut up idiot")
+            # printing the information
+            # print("The media ID is : " + media.media_id_string)
+            # print("The size of the file is : " + str(media.size) + " bytes")
+    except TweepError:
+        print("Error uploading image")
+
+
+
+from tweepy import TweepError
 
 
 def main():
@@ -174,18 +256,8 @@ def main():
         time.sleep(60)
 
 
-def reply_to_user(screen_name, in_reply_to_status_id, movie_name):
-    search_for_movie(movie_name)
-
-
-def reply_to_self():
-    api = create_api()
-    status = api.update_status('Hello joe')
-
-    reply_id = status.id
-    screen_name = status.user.screen_name
-    api.update_status(f'@{screen_name} Hello, just replying to myself!', in_reply_to_status_id=reply_id)
-
-
 if __name__ == "__main__":
     main()
+    # api = create_api()
+    # status = api.update_status("🎬 🕐 ⭐️💰 💵 🎥 🗓️ 🎦 (genre) ✍ 👪")
+    # upload_photo()
